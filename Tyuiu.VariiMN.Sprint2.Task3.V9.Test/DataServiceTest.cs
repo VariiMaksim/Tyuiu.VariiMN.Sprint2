8c9a02aaa394d39133fe7d03d2124f5123235950
Tyuiu.VariiMN@outlook.com <maks.varii@mail.ru>
2025-10-07 17:54:06 +0500

    Task3

---
+++ Tyuiu.VariiMN.Sprint2.Task3.V9.Test/DataServiceTest.cs
@@ -0,0 +1,46 @@
+﻿using Tyuiu.VariiMN.Sprint2.Task3.V9.Lib;
+
+namespace Tyuiu.VariiMN.Sprint2.Task3.V9.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double res = ds.Calculate(x);
+            double wait = 23.12;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+            double res = ds.Calculate(x);
+            double wait = 1;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition3()
+        {
+            DataService ds = new DataService();
+            double x = -3;
+            double res = ds.Calculate(x);
+            double wait = 0.332;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition4()
+        {
+            DataService ds = new DataService();
+            double x = -14;
+            double res = ds.Calculate(x);
+            double wait = -154.071;
+            Assert.AreEqual(wait, res);
+        }
+    }
+
+}
---
+++ Tyuiu.VariiMN.Sprint2.Task3.V9/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.VariiMN.Sprint2.Task3.V9.Lib;
+
+namespace Tyuiu.VariiMN.Sprint2.Task3.V9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Варий М.Н. | ИСПб-25-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #2                                                              *");
+            Console.WriteLine("* Тема: Операции сравнения                                               *");
+            Console.WriteLine("* Задание #3                                                             *");
+            Console.WriteLine("* Вариант #9                                                             *");
+            Console.WriteLine("* Выполнил: Варий Максим Николаевич | ИСПб-25-1                          *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение               *");
+            Console.WriteLine("* функции Y с использованием вложенных оператор if-else,                 *");
+            Console.WriteLine("* где пользователь вводит значение переменной X с клавиатуры.            *");
+            Console.WriteLine("* Округлить полученное значение до трех знаков после запятой.            *");
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            Console.WriteLine("Введите x");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double res = ds.Calculate(x);
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+
+            Console.WriteLine("Значение функции = " + res);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
